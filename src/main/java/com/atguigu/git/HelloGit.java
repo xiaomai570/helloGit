30ff2b57866bad3df4066c46af1b34cbf788bf8d
tom_glb <goodMorning_pro@atguigu.com>
2020-09-06 20:54:53 +0800

    主干添加

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -5,6 +5,8 @@ public class HelloGit {
         System.out.println("Hello Git");
         System.out.println("跟新1");
         System.out.println("创建分支");
+        System.out.println("分支添加");
+        System.out.println("在线更新");
     }
 
 
